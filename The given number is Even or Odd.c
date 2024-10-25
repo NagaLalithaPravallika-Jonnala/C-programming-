56b02ca8224a68972dcb22cbfb3e1b03ef438022
NagaLalithaPravallika-Jonnala <sagarjonnala84@gmail.com>
2024-10-25 12:17:37 +0530

    Create The given number is Even or Odd.c

---
+++ The given number is Even or Odd.c
@@ -0,0 +1,16 @@
+//The given number is Even or Odd
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the value of n:\n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("the given number is Even");
+    }
+    else
+    {
+         printf("the given number is Odd");
+    }
+}
