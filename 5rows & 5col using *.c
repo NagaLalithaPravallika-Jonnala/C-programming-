196e95888934e42657bf938b464d9da0e6498463
NagaLalithaPravallika-Jonnala <sagarjonnala84@gmail.com>
2025-01-03 11:47:33 +0530

    Create 5rows & 5col using *.c

---
+++ 5rows & 5col using *.c
@@ -0,0 +1,15 @@
+//5rows and 5col using *
+#include<stdio.h>
+int main()
+{
+  int i,j;
+  for(i=1;i<=5;i++)
+  {
+      for(j=1;j<=5;j++)
+      {
+          printf("*\t");
+      }
+      printf("\n");
+  }
+ return 0;
+}
